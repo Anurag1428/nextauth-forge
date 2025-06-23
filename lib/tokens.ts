4084fdbb03299475b8a296e4f1e045d6af7570ce
Anurag <anurag789p@gmail.com>
2025-06-24 00:18:07 +0530

    feat: implement generateVerificationToken function and update verification token handling

---
+++ lib/tokens.ts
@@ -0,0 +1,28 @@
+import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { db } from "@/lib/db";
+import { v4 as uuidv4 } from "uuid";
+
+export const generateVerificationToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000); // 1 hour from now
+
+    const existingToken = await getVerificationTokenByEmail(email);
+
+    if (existingToken) {
+        await db.verficationToken.delete({
+            where: {
+                id: existingToken.id,
+            },
+        });
+    }
+
+    const verificationToken = await db.verficationToken.create({
+        data: {
+            email,
+            token,
+            expires,
+        }
+    });
+
+    return verificationToken;
+};
